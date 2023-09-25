97180329161cd2ff8d541efd0aaccc32a634d857
bkmoises <bk_moises@hotmail.com>
2023-09-25 13:59:41 -0300

    criado teste para validar a rota usuários

---
+++ test/user.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+
+const app = require('../src/app');
+
+test('Deve listar todos os usuários', () => {
+  return request(app).get('/users')
+    .then(res => {
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveLength(1);
+      expect(res.body[0]).toHaveProperty('name', 'Moisés Reis');
+    });
+});
+
+test('Deve inserir um usuário com sucesso', () => {
+  return request(app).post('/users')
+    .send({name: "Maria Aparecida", mail: 'maria@mail.com'})
+    .then(res => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe('Maria Aparecida');
+      expect(res.body.mail).toBe('maria@mail.com');
+    });
+});
