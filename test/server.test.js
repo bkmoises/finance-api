fb1fb3009d759e97d20782a9f63767a283b83663
bkmoises <bk_moises@hotmail.com>
2023-09-25 13:59:22 -0300

    criado teste para verificar se o servidor foi inicializado com sucesso

--- test/server.test.js
+++ test/server.test.js
@@ -1,8 +1,6 @@
 const supertest = require('supertest');
-
 const request = supertest('http://localhost:3001');
 
-test('Deve responder na porta 3001', () => {
-  return request.get('/')
-    .then(res => expect(res.status).toBe(200));
-})
+test("Servidor deve rodar na porta 3001", () => {
+ return request.get('/').then(res => expect(res.status).toBe(200));
+});
