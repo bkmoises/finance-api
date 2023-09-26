324af03a0f9abd43081081275f167708076b16d9
bkmoises <bk_moises@hotmail.com>
2023-09-26 13:49:58 -0300

    alteração dos modulos de consulta e criação de usuário para chamar o modulo de serviços

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
 module.exports = (app) => {
-  const findAll = (req, res) => {
-    app.db('users').select()
-      .then(result => res.status(200).json(result))
+  const findAll = (_req, res) => {
+    app.services.user.findAll()
+      .then(result => res.status(200).json(result));
   };
 
   const createUser = async (req, res) => {
-    const result = await app.db('users').insert(req.body, '*')
+    const result = await app.services.user.saveUser(req.body);
     res.status(201).json(result[0]);
   };
 
