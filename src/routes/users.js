62236f224ba90147c1b1adee656ac0350d0f4d25
bkmoises <bk_moises@hotmail.com>
2023-09-26 13:12:26 -0300

    refatoração do teste da rota de usuários para suportar o banco de dados

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,12 @@
-module.exports = () => {
+module.exports = (app) => {
   const findAll = (req, res) => {
-    const users = [
-      { name: 'Moisés Reis', mail: 'moises@mail.com'}
-    ];
-    res.status(200).send(users);
+    app.db('users').select()
+      .then(result => res.status(200).json(result))
   };
 
-  const createUser = (req, res) => {
-    res.status(201).json(req.body);
+  const createUser = async (req, res) => {
+    const result = await app.db('users').insert(req.body, '*')
+    res.status(201).json(result[0]);
   };
 
   return { findAll, createUser };
