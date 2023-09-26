c5345a8ac6720b0fca5abf70f898a600252b5cea
bkmoises <bk_moises@hotmail.com>
2023-09-26 13:35:25 -0300

    inclusão do knex-logger para inpecionar as consultas realizadas

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
 const knexfile = require('../knexfile.js');
+const knexlogger = require('knex-logger');
 
 app.db = knex(knexfile.test);
+
+app.use(knexlogger(app.db));
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/middlewares.js')
