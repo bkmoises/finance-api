343eb40e0b7de73d52c951d412e957aeda37059e
bkmoises <bk_moises@hotmail.com>
2023-09-26 13:09:42 -0300

    criado  o arquivo de configuração do knex framework

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    version: '16.0',
+    connection: {
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'finance'
+    },
+    migrations: {
+      directory: 'src/migrations',
+    },
+    pool: { min: 0, max: 20 },
+  },
+};
