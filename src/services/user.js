9d840c55b82e35f39fecef014034380c3999b5b4
bkmoises <bk_moises@hotmail.com>
2023-09-26 15:03:25 -0300

    inclusão da validação para verificar se o email já foi cadastrado

--- src/services/user.js
+++ src/services/user.js
@@ -1,9 +1,17 @@
 module.exports = (app) => {
-  const findAll = () => {
-    return app.db('users').select();
+  const findAll = (filter = {}) => {
+    return app.db('users').where(filter).select();
   };
 
   const saveUser = async (user) => {
+    if (!user.name) return { error: 'O campo name é requerido.' };
+    if (!user.mail) return { error: 'O campo mail é requerido.' };
+    if (!user.passwd) return { error: 'O campo passwd é requerido.' };
+
+    const userDb = await findAll({ mail: user.mail });
+
+    if (userDb && userDb.length > 0) return { error: 'Email já cadastrado!' };
+
     return app.db('users').insert(user, '*');
   };
 
