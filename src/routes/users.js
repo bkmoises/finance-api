f8f92f0982ca1f6d77b01f23380a79a86ae46ad0
bkmoises <bk_moises@hotmail.com>
2023-09-26 15:02:55 -0300

    inclusão do parametro return para correção do eslint

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,13 @@
 module.exports = (app) => {
   const findAll = (_req, res) => {
     app.services.user.findAll()
-      .then(result => res.status(200).json(result));
+      .then((result) => res.status(200).json(result));
   };
 
   const createUser = async (req, res) => {
     const result = await app.services.user.saveUser(req.body);
-    res.status(201).json(result[0]);
+    if (result.error) return res.status(400).json(result);
+    return res.status(201).json(result[0]);
   };
 
   return { findAll, createUser };
