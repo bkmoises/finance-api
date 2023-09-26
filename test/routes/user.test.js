83aea03e20b218037f189a069964b7fcf7ed1b38
bkmoises <bk_moises@hotmail.com>
2023-09-26 15:03:45 -0300

    inclusão do teste para verificar e-mails duplicado

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -2,21 +2,58 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
+const mail = `${Date.now()}@mail.com`;
+
 it('Deve listar todos os usuários', () => {
   return request(app).get('/users')
-    .then(res => {
+    .then((res) => {
       expect(res.status).toBe(200);
       expect(res.body.length).toBeGreaterThan(0);
     });
 });
 
 it('Deve inserir um usuário com sucesso', () => {
-  const mail = `${Date.now()}@mail.com`
   return request(app).post('/users')
-    .send({name: "Maria Aparecida", mail, passwd: '123' })
-    .then(res => {
+    .send({ name: 'Maria Aparecida', mail, passwd: '123' })
+    .then((res) => {
       expect(res.status).toBe(201);
       expect(res.body.name).toBe('Maria Aparecida');
       expect(res.body.mail).toBe(mail);
+    });
+});
+
+it("Não deve inserir usuário sem nome", () => {
+  return request(app).post('/users')
+    .send({ mail, passwd: '123' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('O campo name é requerido.');
+    });
+});
+
+it('Não deve inserir usuário sem email', () => {
+  return request(app).post('/users')
+    .send({ name: 'Moisés Andrade', passwd: '123' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('O campo mail é requerido.');
+    });
+});
+
+it('Não deve inserir usuário sem senha', () => {
+  return request(app).post('/users')
+    .send({ name: 'Moisés Andrade', mail: 'moises@mail.com' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('O campo passwd é requerido.');
+    });
+});
+
+it('Não deve inserir um e-mail repetido', () => {
+  return request(app).post('/users')
+    .send({ name: 'Josh Kramer', mail, passwd: '123' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Email já cadastrado!');
     });
 });
