0165b8d3611b61e34caf5e37dfd2903c7e62f5df
bkmoises <bk_moises@hotmail.com>
2023-09-27 18:29:03 -0300

    inclusãod do middleware para tratar erro

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,37 +1,37 @@
 module.exports = (app) => {
-  const getAll = (_req, res) => {
+  const getAll = (_req, res, next) => {
     app.services.account.findAll()
       .then((result) => {
         return res.status(200).json(result);
-      });
+      }).catch(err => next(err));
   };
 
-  const get = (req, res) => {
+  const get = (req, res, next) => {
     app.services.account.findById({ id: req.params.id })
       .then((result) => {
         return res.status(200).json(result);
-      });
+      }).catch(err => next(err));
   };
 
-  const createAccount = (req, res) => {
+  const createAccount = (req, res, next) => {
     app.services.account.saveAccount(req.body)
       .then((result) => {
         return res.status(201).json(result[0]);
-      }).catch(err => res.status(400).json({ error: err.message }));
+      }).catch(err => next(err));
   };
 
-  const remove = (req, res) => {
+  const remove = (req, res, next) => {
     app.services.account.deleteAccount({ id: req.params.id })
       .then(() => {
         return res.status(204).json({});
-      });
+      }).catch(err => next(err));
   };
 
-  const update = (req, res) => {
+  const update = (req, res, next) => {
     app.services.account.updateAccount(req.params.id, req.body)
       .then((result) => {
         return res.status(200).json(result[0]);
-      });
+      }).catch(err => next(err));
   };
 
 
