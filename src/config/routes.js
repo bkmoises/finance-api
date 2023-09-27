f5bd34a5ccd69925c0368811c68a5120b7c7d832
bkmoises <bk_moises@hotmail.com>
2023-09-27 11:31:23 -0300

    nclusão da nova rota para trzer o put de contas de usuários

--- src/config/routes.js
+++ src/config/routes.js
@@ -8,5 +8,6 @@ module.exports = (app) => {
     .post(app.routes.accounts.createAccount);
 
   app.route('/accounts/:id')
-    .get(app.routes.accounts.get);
+    .get(app.routes.accounts.get)
+    .put(app.routes.accounts.update);
 };
