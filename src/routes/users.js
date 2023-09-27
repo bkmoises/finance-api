2901d3f17884e47969855cf03892e3d15e05a2a4
bkmoises <bk_moises@hotmail.com>
2023-09-27 10:50:23 -0300

    inclusão do validador

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
-  const findAll = (_req, res) => {
+  const getAll = (_req, res) => {
     app.services.user.findAll()
       .then((result) => res.status(200).json(result));
   };
@@ -10,5 +10,5 @@ module.exports = (app) => {
     return res.status(201).json(result[0]);
   };
 
-  return { findAll, createUser };
+  return { getAll, createUser };
 };
