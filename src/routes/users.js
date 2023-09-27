6b154c1309ea9e1e6ba11d75c54247fe4d6348dd
bkmoises <bk_moises@hotmail.com>
2023-09-27 18:29:17 -0300

    inclusão do middleware para tratar erro

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,17 @@
 module.exports = (app) => {
-  const getAll = (_req, res) => {
+  const getAll = (_req, res, next) => {
     app.services.user.findAll()
-      .then((result) => res.status(200).json(result));
+      .then((result) => res.status(200).json(result))
+      .catch(err => next(err));
   };
 
-  const createUser = async (req, res) => {
+  const createUser = async (req, res, next) => {
     try {
       const result = await app.services.user.saveUser(req.body);
       return res.status(201).json(result[0]);
     }
     catch (err) {
-      return res.status(400).json({ error: err.message });
+      return next(err);
     };
   };
 
