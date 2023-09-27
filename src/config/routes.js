a29536d4843b8b493b07fb04ee700a5fa1a282a2
bkmoises <bk_moises@hotmail.com>
2023-09-27 10:50:45 -0300

    Inclusão da rota para busca de contas por id

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,12 @@
 module.exports = (app) => {
-  app.route('/users') 
-    .get(app.routes.users.findAll)
-    .post(app.routes.users.createUser)
-}
+  app.route('/users')
+    .get(app.routes.users.getAll)
+    .post(app.routes.users.createUser);
+
+  app.route('/accounts')
+    .get(app.routes.accounts.getAll)
+    .post(app.routes.accounts.createAccount);
+
+  app.route('/accounts/:id')
+    .get(app.routes.accounts.get);
+};
