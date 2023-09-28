17954377367771852ea0e27a0fb27fde7879974b
bkmoises <bk_moises@hotmail.com>
2023-09-27 22:11:00 -0300

    inclusão do middleware de authenticação em todas as rotas

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,16 +1,21 @@
 module.exports = (app) => {
+  const authMiddleware = app.config.passport.authenticate();
+
   app.route('/auth/signin')
     .post(app.routes.auth.signin);
 
   app.route('/users')
+    .all(authMiddleware)
     .get(app.routes.users.getAll)
     .post(app.routes.users.createUser);
 
   app.route('/accounts')
+    .all(authMiddleware)
     .get(app.routes.accounts.getAll)
     .post(app.routes.accounts.createAccount);
 
   app.route('/accounts/:id')
+    .all(authMiddleware)
     .get(app.routes.accounts.get)
     .put(app.routes.accounts.update)
     .delete(app.routes.accounts.remove);
