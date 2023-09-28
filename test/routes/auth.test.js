10b7aa1482771d448f1971f29a2ed6639da010d5
bkmoises <bk_moises@hotmail.com>
2023-09-27 21:20:59 -0300

    inclusão dos testes de authenticação

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -15,3 +15,27 @@ it('Deve receber token ao se logar', () => {
       expect(res.body).toHaveProperty('token');
     });
 });
+
+it('Não deve autenticar usuário com senha errada', () => {
+  const mail = `${Date.now()}@mail.com`;
+  return app.services.user.saveUser({
+    name: 'João',
+    mail,
+    passwd: '123456',
+  })
+    .then(() => request(app).post('/auth/signin')
+      .send({ mail, passwd: '123321' }))
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Usuário ou senha invalida!');
+    });
+});
+
+it('Não deve autenticar usuários não cadastrados', () => {
+  return request(app).post('/auth/signin')
+    .send({ mail: 'nãoexiste@mail.com', passwd: '123321' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Usuário ou senha invalida!');
+    });
+});
