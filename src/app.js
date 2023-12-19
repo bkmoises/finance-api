dddd4a0d79bc3fbd9ce294037e214e0456d1c471
bkmoises <bk_moises@hotmail.com>
2023-12-19 12:54:05 -0300

    criação da rota users

--- src/app.js
+++ src/app.js
@@ -4,4 +4,11 @@ app.get('/', (_req, res) => {
   res.status(200).send();
 });
 
+app.get('/users', (_req, res) => {
+  const users = [
+    { name: 'John Doe', email: 'john@mail.com' }
+  ]
+  res.status(200).json(users);
+});
+
 module.exports = app;
