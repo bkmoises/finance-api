0e540ecdefda601d933efb088174fd521c63477b
bkmoises <bk_moises@hotmail.com>
2023-12-20 13:00:42 -0300

    .

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const app = require("express")();
 const consign = require("consign");
 const knex = require("knex");
 const knexfile = require("../knexfile");
+// const knexLogger = require("knex-logger");
 
 // TODO: Criar chavemaento dinânmio
 app.db = knex(knexfile.test);
+
+// app.use(knexLogger(app.db));
 
 consign({ cwd: "src", verbose: false })
   .include("./config/middlewares.js")
